1f18b85a27a15e7d785a5c158747cfd518fd6652
jattinagoudarsanjay <sanjaynjattinagoudar@gmail.com>
2024-01-08 16:17:39 +0530

    file changed

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
-
-def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
-
-   
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
@@ -29,7 +16,7 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with mongodb database.",
+    description="A python package for connecting with database.",
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
@@ -38,5 +25,4 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requiremet("./requirements_dev.txt"),
-)
+    )
